5df4968e059abbd39e0df5f34d56688d412a060e
Paulo <paulocavalcante001@outlook.com>
2022-10-16 19:12:04 -0300

    adicionando lib

---
+++ angular/lib/serialGenerator/serialGenerator.js
@@ -0,0 +1,21 @@
+angular.module("serialGenerator", []);
+angular.module("serialGenerator").provider("serialGenerator", function () {
+  var _lenght = 20;
+  this.getLength = function() {
+      return _lenght;
+  }
+  this.setLength = function(length) {
+    _lenght = length;
+  }
+  this.$get = function () {
+    return {
+      generate: function () {
+        var serial = "";
+        while (serial.length < _lenght) {
+          serial += String.fromCharCode(Math.floor(Math.random() * 64) + 32);
+        }
+        return serial;
+      },
+    };
+  };
+});
